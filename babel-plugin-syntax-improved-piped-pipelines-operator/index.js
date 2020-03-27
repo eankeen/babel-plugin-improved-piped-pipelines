d57d41585658471699ebb2f2389f09fe9f3c30cf
eankeen <24364012+eankeen@users.noreply.github.com>
2020-03-27 12:51:41 -0700

    fix: disalow certain some pipeline operator proposals from being used

--- babel-plugin-syntax-improved-piped-pipelines-operator/index.js
+++ babel-plugin-syntax-improved-piped-pipelines-operator/index.js
@@ -1,7 +1,7 @@
 // mostly taken from babel-plugin-syntax-pipeline-operator
 import { declare } from '@babel/helper-plugin-utils'
 
-export const proposals = ['minimal', 'smart', 'fsharp']
+export const proposals = ['pipeline', 'arrow', 'minimal']
 
 export default declare((api, { proposal }) => {
   api.assertVersion(7)
@@ -13,6 +13,10 @@ export default declare((api, { proposal }) => {
         proposals.join(', ') +
         '. More details: https://github.com/eankeen/babel-improved-piped-pipelines'
     )
+  }
+
+  if (proposal === 'pipeline' || proposal === 'arrow') {
+    throw new Error(`'pipeline' or 'arrow' options not supported yet`)
   }
 
   return {
