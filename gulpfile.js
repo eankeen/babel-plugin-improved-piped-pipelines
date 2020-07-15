b733f0e2e2280ef01ce9bdda79c05d279006cf0c
eankeen <24364012+eankeen@users.noreply.github.com>
2020-07-15 10:19:42 -0700

    fix: change casing of license and readme

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
-import { src, dest } from 'gulp'
+const { src, dest } = require('gulp')
 
-export default async function copyReadme() {
-  await src('readme.md')
-    .pipe(dest('babel-plugin-improved-piped-pipelines'))
-    .pipe(dest('babel-plugin-syntax-improved-piped-pipelines-operator'))
+const plugin = 'babel-plugin-improved-piped-pipelines'
+const syntax = 'babel-plugin-syntax-improved-piped-pipelines-operator'
+
+exports.copyFiles = async function copyFiles() {
+  src('README.md').pipe(dest(plugin)).pipe(dest(syntax))
+
+  src('LICENSE').pipe(dest(plugin)).pipe(dest(syntax))
 }
-
